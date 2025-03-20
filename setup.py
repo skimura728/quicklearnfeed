220ee23b633a211c6cc653a640eb09530002b7c7
Seiji Kimura <kimura.seiji@jp.panasonic.com>
2025-03-20 19:44:47 +0900

    initial

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quicklearnfeed',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'google-generativeai',
+        'feedparser',
+        'requests',
+        'beautifulsoup4',
+    ],
+
+    entry_points="""
+        [console_scripts]
+        quicklearnfeed = quicklearnfeed:main
+    """,
+)
