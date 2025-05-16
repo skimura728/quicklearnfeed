7eae70b6a0180fea52988b616cdf841e71f1e870
Seiji Kimura <kimura.seiji@jp.panasonic.com>
2025-05-17 07:58:48 +0900

    Update URL path for LLM

--- download_model.sh
+++ download_model.sh
@@ -6,6 +6,6 @@ echo "Creating models directory..."
 mkdir -p models
 
 echo "Downloading model..."
-wget https://huggingface.co/TheBloke/Nous-Hermes-2-Mistral-7B-GGUF/resolve/main/nous-hermes-2-mistral-7b.Q2_K.gguf -O models/mistral.gguf
+wget https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q2_K.gguf -O models/mistral.gguf
 
 echo "Model download to models/mistral.gguf"
